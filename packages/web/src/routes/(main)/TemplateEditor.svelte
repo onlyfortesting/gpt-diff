a869768934a2c3ea38881f90b206703501c8c3ab
onlyfortesting <shirolbagas@gmail.com>
2023-11-14 20:01:31 +0700

    fix: main modal not closing bug

--- packages/web/src/routes/(main)/TemplateEditor.svelte
+++ packages/web/src/routes/(main)/TemplateEditor.svelte
@@ -115,6 +115,7 @@
           on:click={() => {
             command.deleteTemplate($template)
             nav.back()
+            $open = false
           }}
           class="py-2.5 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-red-600 text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
         >
